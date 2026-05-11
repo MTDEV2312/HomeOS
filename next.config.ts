3a0ab00ae3a15acdf2d89b6bf2d23c6d731cd691
Mathias Teran <agusmaty23@gmail.com>
2026-05-10 22:27:56 -0500

    fix: corregir configuración de ESLint y TypeScript para no ignorar errores durante la construcción

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.resolve(process.cwd()),
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
 };
 
